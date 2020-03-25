0d57edaddb097dc214a7afafdc801bee810295e4
lauriane <laurianeegouraud@gmail.com>
2020-03-25 16:53:58 +0100

    Ajout de quelques lignes dans strings.xml Modif Edition_des_souvenirs.java / activity_edition_des_souvenirs.xml

--- app/src/main/java/com/ensim/souvenir/Edition_des_souvenirs.java
+++ app/src/main/java/com/ensim/souvenir/Edition_des_souvenirs.java
@@ -5,16 +5,37 @@ import androidx.appcompat.app.AppCompatActivity;
 import android.os.Bundle;
 import android.widget.TextView;
 
+import com.google.android.material.textfield.TextInputEditText;
+
 public class Edition_des_souvenirs extends AppCompatActivity {
 
-    String titre = "Edition des souvenirs";
-    TextView zoneTitre = null;
+    String titre = "Edition des souvenirs"; // valeurs d'un souvenir
+    TextView zoneTitrePage = null;
+
+    String champ_titre = "Koe no Katachi";
+    TextInputEditText zone_titre = null;
+
+    String champ_date = "20/09/2019";
+    TextInputEditText zone_date = null;
+
+    String champ_lieu = "Le Mans";
+    TextInputEditText zone_lieu = null;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_edition_des_souvenirs);
-        zoneTitre = (TextView)findViewById(R.id.TitrePage_Ed_Souvs);
-        zoneTitre.setText(titre);
+        zoneTitrePage = (TextView)findViewById(R.id.TitrePage_Ed_Souvs);
+        zoneTitrePage.setText(titre);
+
+        zone_titre = (TextInputEditText)findViewById(R.id.champ_titre);
+        zone_titre.setText(champ_titre);
+
+        zone_date = (TextInputEditText)findViewById(R.id.champ_date);
+        zone_date.setText(champ_date);
+
+        zone_lieu = (TextInputEditText)findViewById(R.id.champ_lieu);
+        zone_lieu.setText(champ_lieu);
     }
 }
