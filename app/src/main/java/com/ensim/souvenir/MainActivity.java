d2a67e410cb099749a1a30f6e5991b498880fef5
VivienKouamedjo <nanichkouamedjo@gmail.com>
2020-03-28 19:39:27 +0100

    Profile page added

--- app/src/main/java/com/ensim/souvenir/MainActivity.java
+++ app/src/main/java/com/ensim/souvenir/MainActivity.java
@@ -2,7 +2,9 @@ package com.ensim.souvenir;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -10,5 +12,13 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+    }
+
+    public void openFav(View view){
+        Intent fav_data = new Intent(this, FavorisActivity.class);
+        startActivity(fav_data);
+    }
+
+    public void openModeal(View view) {
     }
 }
